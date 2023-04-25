d9f4fcd81abc702538e825988ea11a543f0be527
Aliaksei Yausiuk <Aliakseiyausiuk@gmail.com>
2023-04-25 21:36:44 +0300

    feat: add layout

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import styles from '../../styles/Footer.module.css'
+import { Link } from 'react-router-dom'
+import { ROUTES } from '../../utils/routes'
+import LOGO from '../../images/logo.svg'
+
+const Footer = () => {
+  return (
+    <section className={styles.footer}>
+      <div className={styles.logo}>
+        <Link to={ROUTES.HOME}>
+          <img src={LOGO} alt="Stuff" />
+        </Link>
+      </div>
+      <div className={styles.rights}>
+        Developed
+      </div>
+      <div className={styles.socials}></div>
+    </section>
+  )
+}
+
+export default Footer
