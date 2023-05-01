e2eea5a51032215d9781a95c20a240487fe3c425
Aliaksei Yausiuk <Aliakseiyausiuk@gmail.com>
2023-05-01 23:12:00 +0300

    feat: add component UserForm

---
+++ src/components/User/UserForm.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
+import styles from '../../styles/User.module.css'
+
+import UserSignupForm from './UserSignupForm'
+import { toggleForm } from '../../features/user/userSlice'
+
+const UserForm = () => {
+  const dispatch = useDispatch();
+  const { showForm } = useSelector(({ user }) => user)
+
+  const closeForm = () => dispatch(toggleForm(false))
+
+  return showForm ? (
+    <>
+      <div className={styles.overlay} onClick={closeForm}/>
+      <UserSignupForm closeForm={closeForm}/>
+    </>
+  ) : (<></>);
+}
+
+export default UserForm
