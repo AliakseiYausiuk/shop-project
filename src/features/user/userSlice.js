b56aaf12659e5a640065c3776ac209e113e086b4
Aliaksei Yausiuk <Aliakseiyausiuk@gmail.com>
2023-05-08 14:09:55 +0300

    feat: add remove fn

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -64,6 +64,9 @@ const userSlice = createSlice({
       } else newCart.push({ ...payload, quantity: 1 })
       state.cart = newCart;
     },
+    removeItemFromCart: (state, { payload }) => {
+      state.cart = state.cart.filter(({ id }) => id !== payload);
+    },
     toggleForm: (state, { payload }) => {
       state.showForm = payload;
     },
@@ -78,6 +81,6 @@ const userSlice = createSlice({
   }
 })
 
-export const { addItemToCart, toggleForm, toggleFormType } = userSlice.actions;
+export const { addItemToCart, removeItemFromCart, toggleForm, toggleFormType } = userSlice.actions;
 
 export default userSlice.reducer;
